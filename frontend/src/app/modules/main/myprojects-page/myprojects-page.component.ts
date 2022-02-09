280255c3710e8e595e25ee40cbe81bac67a59d12
KitamiX <Xwernon@ukr.net>
2022-02-09 23:43:17 +0200

    Create My Projects component
    Created My Projects component

---
+++ frontend/src/app/modules/main/myprojects-page/myprojects-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-myprojects-page',
+  templateUrl: './myprojects-page.component.html',
+  styleUrls: ['./myprojects-page.component.sass']
+})
+export class MyprojectsPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
