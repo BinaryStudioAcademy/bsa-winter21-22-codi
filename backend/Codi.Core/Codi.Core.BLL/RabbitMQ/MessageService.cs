6a2eb76ddeedbc1e8a6cfbbe6ce9b21480e5d69a
Vitaliy <vitalik.vinnik.05@gmail.com>
2022-02-11 16:42:41 +0200

    add an example to send single message from WebAPI to RabbitMQ queue

---
+++ backend/Codi.Core/Codi.Core.BLL/RabbitMQ/MessageService.cs
@@ -0,0 +1,32 @@
+﻿using Codi.Core.BL.Interfaces;
+using Codi.RabbitMQ.Interfaces;
+using Codi.RabbitMQ.Models;
+using RabbitMQ.Client;
+
+namespace Codi.Core.BLL.RabbitMQ;
+
+public class MessageService : IMessageService
+{
+    private readonly IMessageProducerScope _messageProducerScope;
+    
+    public MessageService(IMessageProducerScopeFactory messageProducerScopeFactory)
+    {
+        _messageProducerScope = messageProducerScopeFactory.Open(new MessageScopeSettings()
+        {
+            ExchangeName = "WebAPI",
+            ExchangeType = ExchangeType.Direct,
+            QueueName = "Messages",
+            RoutingKey = "message"
+        });
+    }
+
+    public void Dispose()
+    {
+        _messageProducerScope.Dispose();
+    }
+
+    public void SendMessage(string message)
+    {
+        _messageProducerScope.MessageProducer.Send(message);
+    }
+}
