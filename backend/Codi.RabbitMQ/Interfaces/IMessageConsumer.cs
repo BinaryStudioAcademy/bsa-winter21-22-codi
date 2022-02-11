477a0843e62aea61f86d819e7df6960c6edab584
Vitaliy <vitalik.vinnik.05@gmail.com>
2022-02-11 16:40:47 +0200

    add custom RabbitMQ interfaces

---
+++ backend/Codi.RabbitMQ/Interfaces/IMessageConsumer.cs
@@ -0,0 +1,10 @@
+﻿using RabbitMQ.Client.Events;
+
+namespace Codi.RabbitMQ.Interfaces;
+
+public interface IMessageConsumer
+{
+    event EventHandler<BasicDeliverEventArgs> Received;
+    void Connect();
+    void SetAcknowledge(ulong deliveryTag, bool processed);
+}
