6b2859da76bdd6a0a73691e189217c95545a6666
ihorshmoilov <igor.shmoilov@binary-studio.com>
2022-02-15 14:52:57 +0200

    auth service fix

--- frontend/src/app/core/services/auth.service.ts
+++ frontend/src/app/core/services/auth.service.ts
@@ -1,8 +1,8 @@
-import {Injectable} from '@angular/core';
-import { from, switchMap} from "rxjs";
-import {Auth, authState, createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup, updateProfile,} from "@angular/fire/auth";
+import { Injectable } from '@angular/core';
+import { from, switchMap } from "rxjs";
+import { Auth, authState, createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup, updateProfile, } from "@angular/fire/auth";
 import { GoogleAuthProvider, GithubAuthProvider } from "firebase/auth";
-import {idToken} from "rxfire/auth";
+import { idToken } from "rxfire/auth";
 
 @Injectable({
   providedIn: 'root'
@@ -12,22 +12,19 @@ export class AuthService {
   currentUser$ = authState(this.auth);
 
   constructor(private auth: Auth) {
-    this.currentUser$.subscribe( user =>{
+    this.currentUser$.subscribe(user => {
       if (user) {
         user.getIdToken(true).then(token => {
           localStorage.setItem('jwt', token)
         });
       }
-      else {
-        localStorage.removeItem('jwt');
-      }
     })
   }
 
-  signUp(username:string, email: string, password: string) {
+  signUp(username: string, email: string, password: string) {
     return from(createUserWithEmailAndPassword(this.auth, email, password))
       .pipe(
-        switchMap(({user}) => updateProfile(user, {displayName: username}))
+        switchMap(({ user }) => updateProfile(user, { displayName: username }))
       );
   }
 
@@ -49,8 +46,8 @@ export class AuthService {
     return from(this.authLogin(new GithubAuthProvider()))
   }
 
-  authLogin(provider:any) {
-    return from(signInWithPopup(this.auth,provider))
+  authLogin(provider: any) {
+    return from(signInWithPopup(this.auth, provider))
   }
 
   getAuthIdToken() {
