50d70a3e45972e29062ab78bec34436b40c965d4
klm-b <klm_b@outlook.com>
2022-02-15 18:57:54 +0300

    Add mongoDB entities and repositories

---
+++ backend/Codi.Core/Codi.Core.DAL/NoSql/Entities/Abstract/IDocument.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Codi.Core.DAL.NoSql.Entities.Abstract
+{
+    public interface IDocument
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.String)]
+        ObjectId Id { get; set; }
+
+        DateTime CreatedAt { get; }
+    }
+}
---
+++ backend/Codi.Core/Codi.Core.DAL/NoSql/Settings/CodiFileStorageSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace Codi.Core.DAL.NoSql.Settings
+{
+    public class CodiFileStorageSettings
+    {
+        public string ConnectionString { get; set; } = default!;
+        public string DatabaseName { get; set; } = default!;
+    }
+}
