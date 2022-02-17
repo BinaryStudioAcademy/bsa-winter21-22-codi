ec80e60105ccc431e4379cb04cd65233f84fcb1e
SayPlease <saypleaze@outlook.com>
2022-02-17 09:42:35 +0200

    Add register, notification service

--- frontend/src/app/core/services/auth.service.ts
+++ frontend/src/app/core/services/auth.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
-import { from, switchMap } from "rxjs";
-import { Auth, authState, createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup, updateProfile, } from "@angular/fire/auth";
+import { from } from "rxjs";
+import { Auth, authState, createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup } from "@angular/fire/auth";
 import { GoogleAuthProvider, GithubAuthProvider } from "firebase/auth";
 import { idToken } from "rxfire/auth";
+import {Router} from "@angular/router";
+import {NotificationService} from "@core/services/notification.service";
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +13,11 @@ export class AuthService {
 
   currentUser$ = authState(this.auth);
 
-  constructor(private auth: Auth) {
+  constructor(
+    private auth: Auth,
+    private router: Router,
+    private notificationService: NotificationService
+  ) {
     this.currentUser$.subscribe(user => {
       if (user) {
         user.getIdToken(true).then(token => {
@@ -22,14 +28,27 @@ export class AuthService {
   }
 
   signUp(username: string, email: string, password: string) {
-    return from(createUserWithEmailAndPassword(this.auth, email, password))
-      .pipe(
-        switchMap(({ user }) => updateProfile(user, { displayName: username }))
-      );
+    return from(createUserWithEmailAndPassword(this.auth, email, password).then(() => {
+        this.router.navigate(['main']).then(() => {
+          this.notificationService.showSuccessMessage("You have successfully register and logged in", "Welcome back!")
+        })
+      })
+        .catch((error) => {
+          this.notificationService.showErrorMessage(error)
+        })
+    );
   }
 
   signIn(email: string, password: string) {
-    return from(signInWithEmailAndPassword(this.auth, email, password));
+    return from(signInWithEmailAndPassword(this.auth, email, password).then(() => {
+      this.router.navigate(['main']).then(() => {
+        this.notificationService.showSuccessMessage("You have successfully logged in", "Welcome back!")
+      })
+    })
+      .catch((error) => {
+        this.notificationService.showErrorMessage(error)
+      })
+    );
   }
 
   logOut() {
@@ -39,11 +58,25 @@ export class AuthService {
   }
 
   withGoogle() {
-    return from(this.authLogin(new GoogleAuthProvider()))
+    return from(this.authLogin(new GoogleAuthProvider())).subscribe(() => {
+      this.router.navigate(['main']).then(() => {
+        this.notificationService.showSuccessMessage("You have successfully logged in", "Welcome back!")
+      })
+        .catch((error) => {
+          this.notificationService.showErrorMessage(error)
+        })
+    });
   }
 
   withGitHub() {
-    return from(this.authLogin(new GithubAuthProvider()))
+    return from(this.authLogin(new GithubAuthProvider())).subscribe(() => {
+      this.router.navigate(['main']).then(() => {
+        this.notificationService.showSuccessMessage("You have successfully logged in", "Welcome back!")
+      })
+        .catch((error) => {
+          this.notificationService.showErrorMessage(error)
+        })
+    });
   }
 
   authLogin(provider: any) {
