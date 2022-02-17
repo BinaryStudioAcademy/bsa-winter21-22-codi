15129d856930e970f3c36505ec6b79e60f790ebc
klm-b <klm_b@outlook.com>
2022-02-17 15:22:03 +0300

    Add logo to all pages

--- frontend/src/app/modules/main/header/top-nav/top-nav.component.ts
+++ frontend/src/app/modules/main/header/top-nav/top-nav.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthService} from "@core/services/auth.service";
+import {Router} from "@angular/router";
+import { Observable } from 'rxjs';
+import { User } from 'firebase/auth';
 
 @Component({
   selector: 'app-top-nav',
@@ -10,7 +13,10 @@ export class TopNavComponent implements OnInit {
 
   constructor(private authService: AuthService) { }
 
+  $currentUser: Observable<User | null>
+
   ngOnInit(): void {
+    this.$currentUser = this.authService.currentUser$;
   }
 
   logout() {
