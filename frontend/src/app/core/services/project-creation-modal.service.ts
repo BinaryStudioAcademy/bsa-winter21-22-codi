ee064cb1ad07357878acdb873d7fe570fabcc8b6
SayPlease <saypleaze@outlook.com>
2022-02-21 16:38:14 +0200

    Add modal repl

---
+++ frontend/src/app/core/services/project-creation-modal.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
+import {CreateReplModalComponent} from "@shared/components/create-repl-modal/create-repl-modal.component";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProjectCreationModalService {
+
+  constructor(private modalService: NgbModal) {}
+
+  openReplModal() {
+    const modalRef = this.modalService.open(CreateReplModalComponent, {centered: true});
+
+  }
+}
--- frontend/src/app/modules/main/header/top-nav/top-nav.component.ts
+++ frontend/src/app/modules/main/header/top-nav/top-nav.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '@core/services/auth.service';
 import { Observable } from 'rxjs';
 import { User } from 'firebase/auth';
+import {ProjectCreationModalService} from "@core/services/project-creation-modal.service";
 
 @Component({
     selector: 'app-top-nav',
@@ -9,7 +10,7 @@ import { User } from 'firebase/auth';
     styleUrls: ['./top-nav.component.sass'],
 })
 export class TopNavComponent implements OnInit {
-    constructor(private authService: AuthService) { }
+    constructor(private authService: AuthService, private modalService: ProjectCreationModalService) { }
 
     $currentUser: Observable<User | null>
 
@@ -19,5 +20,9 @@ export class TopNavComponent implements OnInit {
 
     logout() {
         this.authService.logOut();
+    }
+
+    createRepl() {
+        this.modalService.openReplModal();
     }
 }
