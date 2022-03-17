2e075601b9b67ed3c402a583332426707564f7f7
KitamiX <Xwernon@ukr.net>
2022-03-17 16:30:42 +0200

    Update workspace-page.component.ts

--- frontend/src/app/modules/main/workspace-page/workspace-page.component.ts
+++ frontend/src/app/modules/main/workspace-page/workspace-page.component.ts
@@ -1,17 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-workspace-page',
-  templateUrl: './workspace-page.component.html',
-  styleUrls: ['./workspace-page.component.sass']
+    selector: 'app-workspace-page',
+    templateUrl: './workspace-page.component.html',
+    styleUrls: ['./workspace-page.component.sass']
 })
-export class WorkspacePageComponent implements OnInit {
-
-  editorOptions = {theme: 'vs-dark', language: 'javascript', scrollBeyondLastLine: false};
-  code: string= 'function x() {\nconsole.log("Hello world!");\n}';
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class WorkspacePageComponent {
+    editorOptions = {theme: 'vs-dark', language: 'javascript', scrollBeyondLastLine: false};
+    code: string= 'function x() {\nconsole.log("Hello world!");\n}';
 }
