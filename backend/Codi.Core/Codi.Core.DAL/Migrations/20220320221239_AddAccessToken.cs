eae6f092390c377d9fcbfbafd94529b3ab3bc6aa
Vitaliy <vitalik.vinnik.05@gmail.com>
2022-03-22 00:14:17 +0200

    add access token to user

---
+++ backend/Codi.Core/Codi.Core.DAL/Migrations/20220320221239_AddAccessToken.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Codi.Core.DAL.Migrations
+{
+    public partial class AddAccessToken : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AccessToken",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AccessToken",
+                table: "Users");
+        }
+    }
+}
