e2c67ff7596912f78242a083c9cc1a0da416ab81
Vitaliy <vitalik.vinnik.05@gmail.com>
2022-03-26 16:58:24 +0200

    fix routes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -8,16 +8,23 @@ const redirectToMain = () => redirectLoggedInTo(['main']);
 
 const routes: Routes = [
     {
-        path: '',
-        loadChildren: () =>
-            import('./modules/landing/landing.module')
-                .then(m => m.LandingModule),
-    },
-    {
         path: 'main',
         loadChildren: () => import('./modules/main/main.module')
             .then(m => m.MainModule),
         ...canActivate(redirectToLogin),
+    },
+    {
+        path: 'workspace',
+        loadChildren: () =>
+            import('./modules/workspace/workspace.module')
+                .then(m => m.WorkspaceModule),
+        ...canActivate(redirectToLogin),
+    },
+    {
+        path: '',
+        loadChildren: () =>
+            import('./modules/landing/landing.module')
+                .then(m => m.LandingModule),
     },
     {
         path: '',
@@ -31,13 +38,6 @@ const routes: Routes = [
         loadChildren: () =>
             import('./modules/user/user.module')
                 .then(m => m.UserModule),
-        ...canActivate(redirectToLogin),
-    },
-    {
-        path: 'workspace',
-        loadChildren: () =>
-            import('./modules/workspace/workspace.module')
-                .then(m => m.WorkspaceModule),
         ...canActivate(redirectToLogin),
     },
     {
