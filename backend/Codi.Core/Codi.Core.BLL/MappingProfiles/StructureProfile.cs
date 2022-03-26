e4f38f589d438ca3fc932c0d9f8788293e999681
Vitaliy <vitalik.vinnik.05@gmail.com>
2022-03-26 20:50:03 +0200

    add backend part of displaying tree

---
+++ backend/Codi.Core/Codi.Core.BLL/MappingProfiles/StructureProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Codi.Core.Common.DTO.Structure;
+using Codi.Core.DAL.NoSql.Entities;
+
+namespace Codi.Core.BLL.MappingProfiles;
+
+public class StructureProfile : Profile
+{
+    public StructureProfile()
+    {
+        CreateMap<FSNode, FSNodeDto>()
+            .ForMember(d => d.Children, opt => opt.MapFrom((s => s.Nodes)));
+        CreateMap<Project, ProjectStructureDto>()
+            .ForMember(d => d.Children, opt => opt.MapFrom(s => s.Nodes));
+    }
+}
