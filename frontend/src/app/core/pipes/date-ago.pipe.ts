438c7041dbe5586f26c25c0b915136f5fc6f40ec
SayPlease <saypleaze@outlook.com>
2022-03-30 11:14:49 +0300

    refact type any

--- frontend/src/app/core/pipes/date-ago.pipe.ts
+++ frontend/src/app/core/pipes/date-ago.pipe.ts
@@ -15,7 +15,7 @@ export class DateAgoPipe implements PipeTransform {
 
                 return 'Just now';
 
-            const intervals: any = {
+            const intervals: {[key: string]: number} = {
                 'year' : 31536000, 'month': 2592000, 'week': 604800,
                 'day': 86400, 'hour': 3600, 'minute': 60, 'second': 1
             };
