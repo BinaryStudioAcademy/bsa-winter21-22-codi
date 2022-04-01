96f6b1b0169174293552394672a33a3ab6bd8ebf
klm-b <klm_b@outlook.com>
2022-04-01 06:17:24 +0300

    Add test client output

--- frontend/src/app/modules/main/main-page/main-page.component.ts
+++ frontend/src/app/modules/main/main-page/main-page.component.ts
@@ -9,24 +9,16 @@ import { NotificationService } from '@core/services/notification.service';
     styleUrls: ['./main-page.component.sass'],
 })
 export class MainComponent implements OnInit, OnDestroy {
-    constructor(private broadcastHub: BroadcastHubService,
-        private buildHub: BuildHubService,
-        private notifications: NotificationService) { }
+    constructor(private broadcastHub: BroadcastHubService) { }
 
     async ngOnInit() {
         await this.broadcastHub.start();
         this.broadcastHub.listenMessages((msg) => {
             console.log(`The next broadcast message was received: ${msg}`);
         });
-
-        await this.buildHub.start();
-        this.buildHub.listenMessages((output) => {
-            this.notifications.showSuccessMessage("Output: " + output.output, "Project id: " + output.projectId.toString());
-        });
     }
 
     ngOnDestroy() {
         this.broadcastHub.stop();
-        this.buildHub.stop();
     }
 }
